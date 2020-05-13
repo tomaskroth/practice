62707200eced6c482a29500413d319d6636f2b11
Tomas Kroth <tomas.kroth@wearewaes.com>
2020-05-13 17:31:25 +0200

    Initial commit for Identity Control Service project

---
+++ ics/src/test/java/com/tk/practice/ics/IcsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tk.practice.ics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
